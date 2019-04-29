b1f837c27d65f29278c9821f53bf6c453e81e418
epixian <epixian@epixian.com>
2019-04-29 11:28:32 -0400

    refactor tailwindcss

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,15 +3,15 @@
 @section ('content')
     <header class="flex items-center mb-6">
         <div class="flex justify-between w-full items-end">
-            <h2 class="text-grey text-sm font-normal">My Projects</h2>
+            <h2 class="text-default text-sm font-normal">My Projects</h2>
             <a href="/projects/create" class="button">New Project</a>
         </div>
     </header>
-
+    
     <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse ($projects as $project)
             <div class="lg:w-1/3 px-3 pb-6">
-                @include ('projects.card')
+                @include ('projects.card') 
             </div>
         @empty
             <div>No projects yet.</div>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,11 +3,23 @@
 @section ('content')
     <header class="flex items-center mb-6">
         <div class="flex justify-between w-full items-end">
-            <p class="text-grey text-sm font-normal">
-                <a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+            <p class="text-default text-sm font-normal">
+                <a href="/projects" class="text-default text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
             </p>
 
-            <a href="{{ $project->path() . '/edit' }}" class="button">Edit Project</a>
+            <div class="flex items-center">
+                <img 
+                    src="{{ gravatar_url($project->owner->email) }}" 
+                    alt="{{ $project->owner->name }}" 
+                    class="rounded-full w-8 mr-2">
+                @foreach ($project->members as $member)
+                    <img 
+                        src="{{ gravatar_url($member->email) }}" 
+                        alt="{{ $member->name }}" 
+                        class="rounded-full w-8 mr-2">
+                @endforeach    
+                <a href="{{ $project->path() . '/edit' }}" class="button ml-4">Edit Project</a>
+            </div>
         </div>
     </header>
 
@@ -16,7 +28,7 @@
 
             <div class="lg:w-3/4 px-3 mb-6">
                 <div class="mb-8">
-                    <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+                    <h2 class="text-lg text-default font-normal mb-3">Tasks</h2>
 
                     {{-- tasks --}}
                     @foreach ($project->tasks as $task)
@@ -25,8 +37,8 @@
                                 @method('PATCH')
                                 @csrf
 
-                                <div class="flex">
-                                    <input name="body" value="{{ $task->body }}" class="w-full {{ $task->completed ? 'text-grey italic' : '' }}">
+                                <div class="flex items-center">
+                                    <input name="body" value="{{ $task->body }}" class="w-full bg-card text-default {{ $task->completed ? 'text-default italic' : '' }}">
                                     <input type="checkbox" name="completed" onChange="this.form.submit();" {{ $task->completed ? 'checked' : '' }}>
                                 </div>
                             </form>
@@ -36,14 +48,14 @@
                         <form method="POST" action="{{ $project->path() . '/tasks' }}">
                             @csrf
 
-                            <input name="body" class="w-full" placeholder="Add a new task..."> 
+                            <input name="body" class="w-full bg-card text-default" placeholder="Add a new task..."> 
                         </form>
                         
                     </div>
                 </div>
                 
                 <div>
-                    <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+                    <h2 class="text-lg text-default font-normal mb-3">General Notes</h2>
 
                     {{-- general notes --}}
                     <form method="POST" action="{{ $project->path() }}">
@@ -59,21 +71,19 @@
                         <button type="submit" class="button">Save</button>
                     </form>
 
-                    @if ($errors->any())
-                        <div class="field mt-6">
-                            @foreach ($errors->all() as $error)
-                                <li class="text-sm text-red">{{ $error }}</li>
-                            @endforeach
-                        </div>
-                    @endif
+                    @include ('errors')
 
                 </div>
             </div>
 
             <div class="lg:w-1/4 px-3">
-                @include ('projects.card')
+                @include ('projects.card') 
 
                 @include ('projects.activity.card')
+
+                @can ('manage', $project)
+                    @include ('projects.invite')
+                @endcan
             </div>    
 
         </div>
