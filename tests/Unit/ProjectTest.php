a4f3e9a2d57b0b0c7368abca0e8e2644d15e81ce
epixian <epixian@epixian.com>
2019-04-29 11:18:18 -0400

    clean up, minor refactor

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -3,8 +3,9 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Project;
+use App\User;
 
 class ProjectTest extends TestCase
 {
@@ -13,7 +14,7 @@ class ProjectTest extends TestCase
     /** @test */
     public function it_has_a_path()
     {
-         $project = factory('App\Project')->create();
+         $project = factory(Project::class)->create();
 
          $this->assertEquals('/projects/' . $project->id, $project->path());
     }
@@ -21,19 +22,29 @@ class ProjectTest extends TestCase
     /** @test */
     public function it_belongs_to_an_owner()
     {
-        $project = factory('App\Project')->create();
+        $project = factory(Project::class)->create();
 
-        $this->assertInstanceOf('App\User', $project->owner);
+        $this->assertInstanceOf(User::class, $project->owner);
     }
 
     /** @test */
     public function it_can_add_a_task()
     {
-        $project = factory('App\Project')->create();
+        $project = factory(Project::class)->create();
 
         $task = $project->addTask('Test task');
 
         $this->assertCount(1, $project->tasks);
         $this->assertTrue($project->tasks->contains($task));
+    }
+
+    /** @test */
+    public function it_can_invite_a_user()
+    {
+        $project = factory(Project::class)->create();
+
+        $project->invite($user = factory(User::class)->create());
+
+        $this->assertTrue($project->members->contains($user));
     }
 }
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,17 +3,42 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Database\Eloquent\Collection;
+use Facades\Tests\Setup\ProjectFactory;
+use App\User;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function a_user_has_projects ()
     {
-        $user = factory('App\User')->create();
+        $user = factory(User::class)->create();
 
         $this->assertInstanceOf(Collection::class, $user->projects); 
+    }
+
+    /** @test */
+    public function a_user_has_accessible_projects()
+    {
+        $john = $this->signIn();
+
+        ProjectFactory::ownedBy($john)->create();
+
+        $this->assertCount(1, $john->assignedProjects());
+
+        $sally = factory(User::class)->create();
+        $nick = factory(User::class)->create();
+
+        $project = ProjectFactory::ownedBy($sally)->create();
+        $project->invite($nick);
+
+        $this->assertCount(1, $john->assignedProjects());
+
+        $project->invite($john);
+
+        $this->assertCount(2, $john->assignedProjects());
     }
 }
