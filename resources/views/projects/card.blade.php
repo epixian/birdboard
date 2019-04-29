5cc9e1d409f85c8a56e5b6fc696346514ed8b8d9
epixian <epixian@epixian.com>
2019-04-29 11:28:41 -0400

    Update card.blade.php

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,7 +1,17 @@
-<div class="card" style="height: 200px;">
+<div class="card flex flex-col" style="height: 200px;">
     <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-light pl-4 mb-2">
-        <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+        <a href="{{ $project->path() }}" class="text-default no-underline">{{ $project->title }}</a>
     </h3>
 
-    <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+    <div class="text-default mb-4 flex-1">{{ str_limit($project->description, 90) }}</div>
+
+    @can ('manage', $project)
+        <footer>
+            <form action="{{ $project->path() }}" method="POST" class="text-right">
+                @csrf
+                @method('DELETE')
+                <button class="text-xs" type="submit">Delete</button>
+            </form>
+        </footer>
+    @endcan
 </div>
