f0631b08a22902e6577729c03898f54f5a33217b
epixian <epixian@epixian.com>
2019-04-29 11:24:17 -0400

    shortened title and description in factory, added notes

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -4,8 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Project::class, function (Faker $faker) {
     return [
-        'title' => $faker->sentence(4),
-        'description' => $faker->sentence(4),
+        'title' => $faker->sentence(1),
+        'description' => $faker->sentence(3),
+        'notes' => $faker->sentence(4),
         'owner_id' => factory(App\User::class)
     ];
 });
